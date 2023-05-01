f18a924efe3586634a3c49d21bbec56832870ce0
RyuseiNa <nakayama_ryusei@retail-ai.jp>
2023-05-01 16:20:13 +0900

    Add routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::prefix('items')->name('item.')->group(function () {
+    Route::get('/', [App\Http\Controllers\ItemController::class, 'list'])->name('list');
+    Route::get('register', [App\Http\Controllers\ItemController::class, 'showRegister'])->name('register');
+    Route::post('register', [App\Http\Controllers\ItemController::class, 'register']);
+    Route::get('{UUID}/update', [App\Http\Controllers\ItemController::class, 'showUpdate'])->name('update');
+    Route::post('{UUID}/update', [App\Http\Controllers\ItemController::class, 'update']);
+    Route::get('{UUID}/delete', [App\Http\Controllers\ItemController::class, 'showDelete'])->name('delete');
+    Route::post('{UUID}/delete', [App\Http\Controllers\ItemController::class, 'delete']);
+});
+Route::prefix('admins')->name('admin.')->group(function () {
+    Route::get('/', [App\Http\Controllers\AdminController::class, 'list'])->name('list');
+    Route::get('{UUID}/update', [App\Http\Controllers\AdminController::class, 'showUpdate'])->name('update');
+    Route::post('{UUID}/update', [App\Http\Controllers\AdminController::class, 'update']);
+    Route::get('{UUID}/delete', [App\Http\Controllers\AdminController::class, 'showDelete'])->name('delete');
+    Route::post('{UUID}/delete', [App\Http\Controllers\AdminController::class, 'delete']);
+});
+
+require __DIR__.'/auth.php';
